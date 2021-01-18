225e643083969eca36df4dfde9ea7fdd0b37cedc
Keith Johnston <kjohnston@cbtnuggets.com>
2021-01-18 15:54:44 -0800

    Delete zip file each time

--- adept/build_mac.sh
+++ adept/build_mac.sh
@@ -2,13 +2,15 @@ tar -zxvf prebuilt/osx/macos-deps-2020-12-22.tar.gz -C /tmp
 tar -zxvf prebuilt/osx/macos-qt-5.15.2-2020-12-22.tar.gz -C /tmp
 
 mkdir cmake_output
-cd cmake_output
+pushd cmakd_output
 cmake -DCMAKE_OSX_DEPLOYMENT_TARGET=10.13 -DQTDIR="/tmp/obsdeps" -DSWIGDIR="/tmp/obsdeps" -DDepsPath="/tmp/obsdeps" -DDISABLE_PYTHON=ON ../..
 make
+popd
 
 # Fix up rpaths for libraries
 ./fix-libs.sh
 
 # Create zip file
 version=`cat version.txt`
+rm -f obs-libs-$version.zip 
 zip obs-libs-$version.zip -r package
